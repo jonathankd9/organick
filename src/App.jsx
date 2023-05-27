f490fa3fb2be6c46b8189220d6d01dee7e5e4130
Jonathan Kofi Donkor <jkd6735@gmail.com>
2023-05-27 14:29:30 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Home } from "./containers";
+import { Home, About } from "./containers";
 
 function App() {
 	const [count, setCount] = useState(0);
@@ -10,6 +10,7 @@ function App() {
 	return (
 		<>
 			<Home />
+			<About />
 		</>
 	);
 }
