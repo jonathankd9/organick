97964f188c2eb2a9749331d3c09c32d463c14180
Jonathan Kofi Donkor <jkd6735@gmail.com>
2023-05-27 14:53:54 +0000

    update

---
+++ src/containers/PasswordProtect/PasswordProtect.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PasswordProtect = () => {
+	return <div>PasswordProtect</div>;
+};
+
+export default PasswordProtect;
---
+++ src/containers/Shop_single/Shop_single.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Shop_single = () => {
+	return <div>Shop_single</div>;
+};
+
+export default Shop_single;
