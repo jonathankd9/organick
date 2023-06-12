e53e589ab7a6b623bb05c11d1616bf3ac6a71cb7
nii_okantey <nunoogideon19@gmail.com>
2023-06-12 12:05:28 +0000

    deleted app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 import { Home, About } from "./containers";
 
 function App() {
-	const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-	return (
-		<>
-			<Home />
-		</>
-	);
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
