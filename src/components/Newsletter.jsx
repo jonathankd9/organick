e23ff7b47b5c2e1f99d4d4c5170f30fb9f7ff7ff
Jonathan Kofi Donkor <jkd6735@gmail.com>
2023-06-19 07:22:32 +0000

    update

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -3,14 +3,16 @@ import React from "react";
 const Newsletter = () => {
 	return (
 		<div>
-			<div className="container flex items-center gap-10 bg-[url('/src/assets/newsletter/subscribe-bg.jpg')] my-10 p-20 rounded-2xl	">
-				<div className="flex-initial w-2/4">
-					<h2 className="text-white font-bold leading-none">Subscribe to </h2>
-					<h2 className="text-white font-bold leading-none">our Newsletter </h2>
+			<div className="container md:grid md:grid-cols-2 md:items-center sm:flex sm:flex-col sm:items-center sm:justify-center gap-10 bg-[url('/src/assets/newsletter/subscribe-bg.jpg')] my-10 p-14 rounded-2xl	">
+				<div className="flex-initial">
+					<h2 className="text-white font-bold leading-none md:text-[50px] md:text-left sm:text-[32px] sm:text-center">
+						Subscribe to <br /> our Newsletter
+					</h2>
+					{/* <h2 className="text-white font-bold leading-none"></h2> */}
 				</div>
-				<div className="flex gap-1">
+				<div className="md:flex md:gap-1 sm:grid sm:grid-cols-1 sm:gap-3">
 					<input
-						class="flex-grow-1 h-20 placeholder:italic placeholder:text-slate-400 block bg-white w-64 border border-slate-300 rounded-2xl pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+						class="flex-grow-1 h-20 placeholder:italic placeholder:text-slate-400 block bg-white w-64 border border-slate-300 rounded-2xl pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm sm:w-full"
 						placeholder="Your Email Address..."
 						type="text"
 						name="search"
