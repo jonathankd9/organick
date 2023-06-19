7a96a1a87ed3a0c387498195e6f8c4fcd48d07ca
nii_okantey <nunoogideon19@gmail.com>
2023-06-19 10:12:22 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import React from "react";
-import { Home } from "./containers";
+import {
+  Home,
+  About,
+  Blog,
+  Changelog,
+  Contact,
+  Error404,
+  Licenses,
+  PasswordProtect,
+  Portfolio,
+  Service,
+  Shop,
+  ShopSingle,
+  Team,
+} from "./containers";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
-	return (
-		<div>
-			<Home />
-		</div>
-	);
+  return (
+    <React.Fragment>
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </React.Fragment>
+  );
 };
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,39 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { NavData } from "./NavData";
+import logo from "../assets/Logo.svg";
+import { Link, NavLink } from "react-router-dom";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="md:flex justify-between items-center py-2">
+      <div>
+        <img src={logo} alt="organick-logo" />
+      </div>
+      <div className="md:flex gap-4 items-center">
+        {NavData.map((element) => {
+          return (
+            <NavLink to={element.link} key={element.id}>
+              {element.name}
+            </NavLink>
+          );
+        })}
+      </div>
+      <div className="md:flex gap-4 items-center">
+        <div className="bg-secondary p-2 rounded-full">
+          <SearchIcon className="text-pastel" />
+        </div>
+        <div className="flex items-center gap-1 ">
+          <div className="bg-primary p-2 rounded-full">
+            <ShoppingCartOutlinedIcon className="text-pastel" />
+          </div>
+          <Link>Cart(0)</Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
