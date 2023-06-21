a5cece1ee6c47008da434d6db33998cae313a5bc
Jonathan Kofi Donkor <jkd6735@gmail.com>
2023-06-21 09:11:12 +0000

    update

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = ({ image, name }) => {
+	return (
+		<div
+			className="bg-cover bg-center w-full flex items-center justify-center"
+			style={{
+				backgroundImage: `url(${image})`,
+			}}
+		>
+			<h1 className="font-extrabold text-[70px]">{name} </h1>
+		</div>
+	);
+};
+
+export default Header;
--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -1,44 +1,49 @@
 import React from "react";
-import { Navbar, Footer, Newsletter } from "./../components";
+import { Navbar, Header, Footer, Newsletter } from "./../components";
 import { WwoyData } from "./../components/data/WwoyData";
+import AboutBanner from "./../assets/header/aboutus.png";
 
 const About = () => {
-  return (
-    <>
-      {/* Navbar  */}
-      <Navbar />
-      {/* What we offer for you */}
-      <div className="bg-primary my-20 md:py-[10rem] sm:py-[5rem]">
-        <div className="container bg-primary flex flex-col justify-center items-center ">
-          <div className="text-center">
-            <p className="tail text-white">About us</p>
-            <h2 className="text-white mb-5">What We Offer for You</h2>
-          </div>
-          <div className="flex flex-wrap gap-5 sm:justify-center">
-            {WwoyData.map((wwoy) => (
-              <div className="" key={wwoy.id}>
-                <div className="h-72 w-72 rounded-2xl">
-                  <img
-                    className="h-full w-full object-cover rounded-2xl"
-                    src={wwoy.image}
-                    alt=""
-                  />
-                </div>
-                <p className="text-white text-center font-medium font-roboto my-[1.5rem] text-[25px] ">
-                  {wwoy.name}
-                </p>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
+	return (
+		<>
+			{/* Navbar  */}
+			<Navbar />
 
-      {/* Subscribe to our newsletter */}
-      <Newsletter />
-      {/* Footer */}
-      <Footer />
-    </>
-  );
+			{/* Header  */}
+			<Header name="About us" image={AboutBanner} />
+
+			{/* What we offer for you */}
+			<div className="bg-primary my-20 md:py-[10rem] sm:py-[5rem]">
+				<div className="container bg-primary flex flex-col justify-center items-center ">
+					<div className="text-center">
+						<p className="tail text-white">About us</p>
+						<h2 className="text-white mb-5">What We Offer for You</h2>
+					</div>
+					<div className="flex flex-wrap gap-5 sm:justify-center">
+						{WwoyData.map((wwoy) => (
+							<div className="" key={wwoy.id}>
+								<div className="h-72 w-72 rounded-2xl">
+									<img
+										className="h-full w-full object-cover rounded-2xl"
+										src={wwoy.image}
+										alt=""
+									/>
+								</div>
+								<p className="text-white text-center font-medium font-roboto my-[1.5rem] text-[25px] ">
+									{wwoy.name}
+								</p>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+
+			{/* Subscribe to our newsletter */}
+			<Newsletter />
+			{/* Footer */}
+			<Footer />
+		</>
+	);
 };
 
 export default About;
