107e5f3caa3817f85ffc305b18f024c919a942a1
Jonathan Kofi Donkor <jkd6735@gmail.com>
2023-06-26 00:11:53 +0000

    update

--- src/components/data/NavData.jsx
+++ src/components/data/NavData.jsx
@@ -1,9 +1,9 @@
 export const NavData = [
-  { id: 1, name: "Home", link: "/" },
-  { id: 2, name: "About", link: "/about" },
-  { id: 3, name: "Pages", link: "/pages" },
-  { id: 4, name: "Shop", link: "/shop" },
-  { id: 5, name: "Projects", link: "/projects" },
-  { id: 6, name: "News", link: "/news" },
-  { id: 7, name: "Team", link: "/team" },
+	{ id: 1, name: "Home", link: "/" },
+	{ id: 2, name: "About", link: "/about" },
+	{ id: 3, name: "Pages", link: "/pages" },
+	{ id: 4, name: "Shop", link: "/shop" },
+	// { id: 5, name: "Projects", link: "/projects" },
+	// { id: 6, name: "News", link: "/news" },
+	{ id: 7, name: "Team", link: "/team" },
 ];
--- src/containers/Shop.jsx
+++ src/containers/Shop.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import ShopBanner from "./../assets/header/shop.png";
 
 const Shop = () => {
-	return <div>Shop</div>;
+	return (
+		<div>
+			{/* Navbar  */}
+			<Navbar />
+
+			{/* Header */}
+			<Header name="About us" image={ShopBanner} />
+			{/* Subscribe to our newsletter */}
+			<Newsletter />
+			{/* Footer */}
+			<Footer />
+		</div>
+	);
 };
 
 export default Shop;
