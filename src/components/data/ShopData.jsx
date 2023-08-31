11b3ed23ad4157255ee7338204c86ac3112b3558
Jonathan Kofi Donkor <jkd6735@gmail.com>
2023-08-31 06:07:17 +0000

    update

---
+++ src/components/data/ShopData.jsx
@@ -0,0 +1,11 @@
+import Spicy from "./../../assets/wwoy/spicy.jpg";
+import Nuts from "./../../assets/wwoy/nuts.jpg";
+import Fruits from "./../../assets/wwoy/fruits.jpg";
+import Vegetables from "./../../assets/wwoy/vegetable.jpg";
+
+export const ShopData = [
+	{ id: 1, name: "Spicy", image: Spicy },
+	{ id: 2, name: "Nuts and Feesd", image: Nuts },
+	{ id: 3, name: "Fruits", image: Fruits },
+	{ id: 4, name: "Vegetables", image: Vegetables },
+];
