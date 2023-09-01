87a3b58ef6d0f707d849e5874167cf8571cd453b
Jonathan Kofi Donkor <jkd6735@gmail.com>
2023-09-01 20:11:56 +0000

    update

--- src/components/data/ShopData.jsx
+++ src/components/data/ShopData.jsx
@@ -1,17 +1,29 @@
 import Spicy from "./../../assets/wwoy/spicy.jpg";
 
+import Brown_Hazelnut from "../../assets/shop/brown-hazelnut.png";
+import Calabrese from "../../assets/shop/calabrese.png";
+import Eggs from "../../assets/shop/eggs.png";
+import Fresh_Banana from "../../assets/shop/fresh-banana.png";
+import Fresh_Corn from "../../assets/shop/fresh-corn.png";
+import LightMungBean from "../../assets/shop/light-mung-bean.png";
+import MungBean from "../../assets/shop/mung-bean.png";
+import OrganicAlmonds from "../../assets/shop/organic-almonds.png";
+import VeganRed from "../../assets/shop/vegan-red.png";
+import WhiteHazelnut from "../../assets/shop/white-hazelnut.png";
+import WhiteNuts from "../../assets/shop/white-nuts.png";
+import Zelco from "../../assets/shop/zelco-suji.png";
 
 export const ShopData = [
 	{id: 1, name: "Calabrese Broccoli", image: Calabrese},
-	{id: 2, name: "Fresh Banana Fruites", image: Nuts},
-	{id: 3, name: "White Nuts", image: Fruits},
-	{id: 4, name: "Vegan Red Tomato", image: Vegetables},
-	{id: 5, name: "Mung Bean", image: Vegetables},
-	{id: 6, name: "Brown Hazelnut", image: Vegetables},
-	{id: 7, name: "Eggs", image: Vegetables},
-	{id: 8, name: "Zelco Suji Elaichi Rusk", image: Vegetables},
-	{id: 9, name: "Mung Bean", image: Vegetables},
-	{id: 10, name: "White Hazelnut", image: Vegetables},
-	{id: 10, name: "Fresh Corn", image: Vegetables},
-	{id: 10, name: "Organic Almonds", image: Vegetables},
+	{id: 2, name: "Fresh Banana Fruites", image: Fresh_Banana},
+	{id: 3, name: "White Nuts", image: WhiteNuts},
+	{id: 4, name: "Vegan Red Tomato", image: VeganRed},
+	{id: 5, name: "Mung Bean", image: LightMungBean},
+	{id: 6, name: "Brown Hazelnut", image: Brown_Hazelnut},
+	{id: 7, name: "Eggs", image: Eggs},
+	{id: 8, name: "Zelco Suji Elaichi Rusk", image: Zelco},
+	{id: 9, name: "Mung Bean", image: MungBean},
+	{id: 10, name: "White Hazelnut", image: WhiteHazelnut},
+	{id: 10, name: "Fresh Corn", image: Fresh_Corn},
+	{id: 10, name: "Organic Almonds", image: OrganicAlmonds},
 ];
