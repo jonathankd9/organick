e5f566ca16f78cacf68db9e5fe6d29f858f2f756
Jonathan Kofi Donkor <jkd6735@gmail.com>
2023-09-01 20:03:52 +0000

    update

--- src/components/data/ShopData.jsx
+++ src/components/data/ShopData.jsx
@@ -1,11 +1,17 @@
 import Spicy from "./../../assets/wwoy/spicy.jpg";
-import Nuts from "./../../assets/wwoy/nuts.jpg";
-import Fruits from "./../../assets/wwoy/fruits.jpg";
-import Vegetables from "./../../assets/wwoy/vegetable.jpg";
+
 
 export const ShopData = [
-	{ id: 1, name: "Spicy", image: Spicy },
-	{ id: 2, name: "Nuts and Feesd", image: Nuts },
-	{ id: 3, name: "Fruits", image: Fruits },
-	{ id: 4, name: "Vegetables", image: Vegetables },
+	{id: 1, name: "Calabrese Broccoli", image: Calabrese},
+	{id: 2, name: "Fresh Banana Fruites", image: Nuts},
+	{id: 3, name: "White Nuts", image: Fruits},
+	{id: 4, name: "Vegan Red Tomato", image: Vegetables},
+	{id: 5, name: "Mung Bean", image: Vegetables},
+	{id: 6, name: "Brown Hazelnut", image: Vegetables},
+	{id: 7, name: "Eggs", image: Vegetables},
+	{id: 8, name: "Zelco Suji Elaichi Rusk", image: Vegetables},
+	{id: 9, name: "Mung Bean", image: Vegetables},
+	{id: 10, name: "White Hazelnut", image: Vegetables},
+	{id: 10, name: "Fresh Corn", image: Vegetables},
+	{id: 10, name: "Organic Almonds", image: Vegetables},
 ];
